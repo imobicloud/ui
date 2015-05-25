6b27ab89db0327870490336b46feba216a0bc664
Manuel Conde Vendrell <manu_conde@hotmail.com>
2015-05-25 16:53:08 +0200

    Commented to explain how it works, no code fixes
    Fixed only some explained params that were obsolete

--- com.imobicloud.calendar/controllers/calendar.js
+++ com.imobicloud.calendar/controllers/calendar.js
@@ -7,7 +7,8 @@ init(arguments[0]);
 /*
  args = {
  	date: moment object,
- 	dateFormatter: null // or function(){}
+ 	weekFormatter: null or function(){}
+ 	dateFormatter: null or function(){}
  }
  * */
 function init(args) {
@@ -17,6 +18,7 @@ function init(args) {
 	loadDate(args.date, args.dateFormatter || dateFormatter);
 };
 
+// Creates the week of day row. Will use the default weekFormatter if you don't pass your own function
 function loadWeek(formatter) {
   	var dow = firstDayOfWeek,
   		weekNames = moment.weekdaysShort(),
@@ -33,11 +35,12 @@ function loadWeek(formatter) {
 	$.calendar.add(container);
 }
 
+// Creates the month with six rows. Will use the default dateFormatter if you don't pass your own function
 function loadDate(time, formatter) {
 	var column      = 0,
 		thisMonth   = time.month(),
   		currentDate = time.subtract(firstDayOfWeek === 0 ? time.day() : ( time.day() || 7 ) - 1, 'days'),
-  		todayId     = moment().startOf('day').format();// get today's iso string without hours, minutes and seconds
+  		todayId     = moment().startOf('day').format(); // get today's iso string without hours, minutes and seconds
   	
   	var container = $.UI.create('View', { classes: 'imc-calendar-dates' });
   	
@@ -54,6 +57,7 @@ function loadDate(time, formatter) {
   			isThisMonth = false;
   		}
   		
+        // Set some custom properties
 		container.add(formatter({
 			index: i,
 			column: column,
@@ -73,10 +77,11 @@ function loadDate(time, formatter) {
 
 /*
  params = {
- 	column: 0,
- 	weekText: 31
+    column: 0, // from 0 to 6
+   	weekText: "Sun"
  } 
  * */
+// Standard weekFormatter includes a view for wrapper and a label for text
 function weekFormatter(params) {
   	var vDate = $.UI.create('View', { classes: 'imc-calendar-week imc-calendar-week-' + params.column });
 		vDate.add( $.UI.create('Label', { text: params.weekText, classes: 'imc-calendar-week-label imc-calendar-week-label-' + params.column }) );
@@ -86,13 +91,16 @@ function weekFormatter(params) {
 /*
  params = {
  	index: 0,  // from 0 to 41, 41 dates of a month view, row = Math.floor(params.index / 7)
- 	column: 0,
+ 	column: 0, // from 0 to 6
  	dateId: "2015-04-23T00:00:00+07:00", // iso string with timezone
  	dateText: 31,
  	isThisMonth: true,
  	isToday: false
  } 
  * */
+// Standard dateFormatter includes a view for wrapper and a label for text
+// You can differentiate between: days in current month, the current day (today class)
+// and days out of the month (disabled class)
 function dateFormatter(params) {
   	var  viewClasses = ['imc-calendar-date'],
 		labelClasses = ['imc-calendar-date-label'];
@@ -110,8 +118,8 @@ function dateFormatter(params) {
 	 viewClasses.push('imc-calendar-date-' + params.column);
 	labelClasses.push('imc-calendar-date-label-' + params.column);
 	
-	var vDate = $.UI.create('View',     { date: params.dateId,   classes:  viewClasses.join(' ') });
-		vDate.add( $.UI.create('Label', { text: params.dateText, classes: labelClasses.join(' ') }) );
+	var vDate = $.UI.create('View', { date: params.dateId,   classes:  viewClasses.join(' ') });
+    vDate.add( $.UI.create('Label', { text: params.dateText, classes: labelClasses.join(' ') }) );
 		
 	return vDate;
 }
