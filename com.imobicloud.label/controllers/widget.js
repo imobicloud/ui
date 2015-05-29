a3b79e04cb1f95f419cdf0736cff3acc26da4e9d
Pham Thanh Quang <ptquang86@gmail.com>
2015-05-29 14:32:08 +0700

    com.imobicloud.label: fix Android
    label.html does not support custom font, use default font instead

--- com.imobicloud.label/controllers/widget.js
+++ com.imobicloud.label/controllers/widget.js
@@ -9,69 +9,111 @@ exports.setText = init;
 
 /*
 args = {
-	// ... tss styles
- 	text: 		'',
- 	textColor: 	'#ffffff',
- 	search: 	[{ 'text': '', 'color': '#ffffff', 'font': { 'fontSize': 18, 'fontFamily': 'HelveticaNeue-Medium' } }]
+    // ... tss styles
+    text:       '',
+    textColor:  '#ffffff',
+    search:     [{ 'text': '', 'color': '#ffffff', 'font': { 'fontSize': 18, 'fontFamily': 'HelveticaNeue-Medium' } }],
+    duplicate:  true | false,
+    case_sensitive: true | false 
 }
  * */
 function formatText(args) {
-	var exclude = ['id', 'text', 'textColor', 'search'];
-	$.label.applyProperties( _.omit(args, exclude) );
-	
-	var text = args.text,
-		search;
-	
-	if (args.search) {
-		if (typeof(args.search) != 'string') {
-			search = args.search;
-		} else {
-			search = JSON.parse( args.search.replace(/\'/g, '"') );
-		}
-	} else {
-		search = [];
-	}
-		
-	if (OS_IOS) {
-		var attributedString = Ti.UI.iOS.createAttributedString({ text: text });
-		
-		if (args.textColor) {
-			attributedString.addAttribute({
-	           	type: Ti.UI.iOS.ATTRIBUTE_FOREGROUND_COLOR,
-    			value: args.textColor,
-    			range: [0, text.length]
-	       	});
-		}
-		
-		for(var i=0,j=search.length; i<j; i++){
-		  	var s = search[i],
-		  		sText   = s.text + '',
-		  		sIndex  = text.indexOf(sText), 
-		  		sLength = sText.length;
-		  		
-			s.font && attributedString.addAttribute({
-	            type: Ti.UI.iOS.ATTRIBUTE_FONT,
-	            value: s.font,
-	            range: [sIndex, sLength]
-	       	});
-	       	
-	       	s.color && attributedString.addAttribute({
-	            type: Ti.UI.iOS.ATTRIBUTE_FOREGROUND_COLOR,
-    			value: s.color,
-    			range: [sIndex, sLength]
-	       	});
-		};
-		
-		$.label.attributedString = attributedString;
-	} else {
-		for(var i=0,j=search.length; i<j; i++){
-			var s = search[i],
-				face  = s.font  ? ('face="' + s.font.fontFamily + '"') : '',
-				color = s.color ? ('color="' + s.color + '"') 			: '';
-			text = text.replace(sText, '<font ' + face + ' '+ color + '>' + sText + '</font>');
-		}
+    var exclude = ['id', 'text', 'textColor', 'search'];
+    $.label.applyProperties( _.omit(args, exclude) );
+    
+    var text = args.text,
+       _text = args.case_sensitive ? text.toLowerCase() : text,
+        search, s, sText;
+    
+    if (args.search) {
+        if (typeof(args.search) != 'string') {
+            search = args.search;
+        } else {
+            search = JSON.parse( args.search.replace(/\'/g, '"') );
+        }
+    } else {
+        search = [];
+    }
+        
+    if (OS_IOS) {
+        var attributedString = Ti.UI.iOS.createAttributedString({ text: text }),
+            startAt = 0,
+            sIndex, sLength;
+        
+        if (args.textColor) {
+            attributedString.addAttribute({
+                type: Ti.UI.iOS.ATTRIBUTE_FOREGROUND_COLOR,
+                value: args.textColor,
+                range: [0, text.length]
+            });
+        }
+        
+        for(var i=0,j=search.length; i<j; i++) {
+            s = search[i];
+            sText = (args.case_sensitive) ? (s.text + '').toLowerCase(): s.text + '';
+            sIndex  = _text.indexOf(sText, startAt);
+                          
+            sLength = sText.length;
+            
+            s.font && attributedString.addAttribute({
+                type: Ti.UI.iOS.ATTRIBUTE_FONT,
+                value: s.font,
+                range: [sIndex, sLength]
+            });
+            
+            s.color && attributedString.addAttribute({
+                type: Ti.UI.iOS.ATTRIBUTE_FOREGROUND_COLOR,
+                value: s.color,
+                range: [sIndex, sLength]
+            });
+            
+            if (args.duplicate == false && sIndex > -1) {
+                startAt += sIndex + sLength;
+            };
+        };
+        
+        $.label.attributedString = attributedString;
+    } else {
+        var html     = '',
+            tempText = text,
+            sIndex   = -1,
+            hasBold = false;
+        
+        for(var i = 0, j = search.length; i < j; i++) {
+            s     = search[i];
+            sText = s.text + '';
+            
+            if (args.case_sensitive) {
+                sText   = sText.toLowerCase();
+                sIndex  = _text.indexOf(sText);
+                sText   = text.substring(sIndex, sIndex + sText.length);
+            } else {
+                sIndex = tempText.indexOf(sText);
+            }
 
-		args.textColor && ($.label.color = args.textColor);
-		$.label.html = text;
-	}
+			// Android - Html does not support custom font
+			// Use default font instead
+			var isBold = false;
+			if (s.font && ( s.font.fontFamily.indexOf('-Bold') != -1 || s.font.fontFamily.indexOf('-Medium') != -1 )) {
+				isBold = true;
+				if (hasBold === false) {
+					$.label.font.fontFamily = 'sans-serif';
+					hasBold = true;
+				}
+			}
+			
+			var _html = (s.color ? '<font color=' + s.color + '>' : '') + (isBold ? '<b>' : '') + sText + (isBold ? '</b>' : '') + (s.color ? '</font>' : '');
+
+            if (args.duplicate == false) {
+                html += tempText.substring(0, sIndex + sText.length).replace(sText, _html);
+                tempText = tempText.substring(sIndex + sText.length, tempText.length);
+            } else {
+                html = tempText.replace(sText, _html);
+            }
+        }
+
+        args.textColor && ($.label.color = args.textColor);
+        $.label.html = html;
+        $.label.value = text;
+    }
 }
